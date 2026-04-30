ed5c1e4222942b352ec03d704ba9baea4f6fa5fa
mahmoudkamalfci <mahmoudkamal01099@gmail.com>
2026-04-30 16:43:23 +0300

    feat: configure image optimization for SVGs and add placehold.co to remote patterns

--- apps/frontend/next.config.js
+++ apps/frontend/next.config.js
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
+    contentDispositionType: 'attachment',
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
       },
     ],
   },
